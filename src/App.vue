0c2101d3c6abfef6406d37a61d6476f0e4071aa2
aslan-alt <874470891@qq.com>
2020-01-14 18:45:38 +0800

    starry-sky

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="stars">
+      <div class="start">
+        <a href="www.baidu.com">
+          <svg class="icon">
+            <use xlink:href="#icon-icon-test" />
+          </svg>
+        </a>
+      </div>
+      <div v-for="(item,index) in starsCount" :key="index" class="star" ref="star"></div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      starsCount: 800,
+      distance: 800
+    };
+  },
+  mounted() {
+    let _this = this;
+    let starArray = _this.$refs.star;
+    console.log(starArray);
+    starArray.forEach(item => {
+      var speed = 0.2 + Math.random() * 1;
+      var thisDistance = _this.distance + Math.random() * 300;
+      item.style.transformOrigin = `0 0 ${thisDistance}px`;
+      item.style.transform = `translate3d(0,0,-${thisDistance}px) rotateY(${Math.random() *
+        360}deg) rotateX(${Math.random() * -50}deg) scale(${speed},${speed})`;
+    });
+  }
+};
+</script>
+<style>
+@keyframes rotate {
+  0% {
+    transform: perspective(400px) rotateZ(20deg) rotateX(-400deg) rotateY(0);
+  }
+
+  100% {
+    transform: perspective(400px) rotateZ(20deg) rotateX(-400deg)
+      rotateY(-360deg);
   }
 }
-</script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.stars {
+  transform: perspective(500px);
+  transform-style: preserve-3d;
+  position: absolute;
+  perspective-origin: 50% 100%;
+  left: 50%;
+  animation: rotate 90s infinite linear;
+  bottom: 0;
+}
+
+.star {
+  width: 4px;
+  height: 4px;
+  background: #f7f7b8;
+  position: absolute;
+  top: 0;
+  left: 0;
+  backface-visibility: hidden;
+}
+.start {
+  position: absolute;
+  bottom: 150px;
 }
 </style>
